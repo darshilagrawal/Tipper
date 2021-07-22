58e329c575fd00754f480438a1a1bd34d40a8e63
darshilagrawal <darshilagrawal2000@gmail.com>
2021-07-22 23:30:13 +0530

    Adding Lottie Animations
    Adding Custom Color Set Lottie animations to enhance user experience

--- Tipsy/Controller/ResultViewController.swift
+++ Tipsy/Controller/ResultViewController.swift
@@ -6,11 +6,13 @@
 //
 
 import UIKit
-
+import Lottie
 class ResultViewController: UIViewController {
 
     @IBOutlet weak var settingsLabel: UILabel!
     @IBOutlet weak var totalLabel: UILabel!
+    
+    @IBOutlet weak var tipAnimation: AnimationView!
     var result = "0.0"
     var tip = 10
     var split = 2
@@ -20,7 +22,13 @@ class ResultViewController: UIViewController {
         settingsLabel.text = "Split between \(split) people, with \(tip)% tip"
         // Do any additional setup after loading the view.
     }
-    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tipAnimation.contentMode = .scaleAspectFit
+        tipAnimation.loopMode = .loop
+        tipAnimation.animationSpeed = 0.5
+        tipAnimation.play()
+    }
     @IBAction func reCalculatePressed(_ sender: UIButton) {
         UIView.transition(with: sender,
                                   duration: 1,
